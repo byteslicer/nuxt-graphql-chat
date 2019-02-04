d462510818516645b42552ee82ea0ac621f65c57
Lazzrek <me.teichmann@gmail.com>
2019-02-04 21:50:45 +0100

    Added knex and db functions

---
+++ migrations/20190203172528_init.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+      knex.schema.createTable('users', table => {
+      table.uuid('id').primary().notNullable()
+      table.string('name').unique().notNullable()
+      table.string('password', 64).notNullable()
+      table.string('salt', 32).notNullable()
+      table.datetime('createdAt').defaultTo(knex.fn.now())
+    }),
+
+    knex.schema.createTable('messages', table => {
+      table.uuid('id').primary().notNullable()
+      table.uuid('userId')
+      table.text('content')
+      table.datetime('createdAt').defaultTo(knex.fn.now())
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('messages')
+  ])
+};
