253b3152abd81ed944a98c0fb3d04ca71e7e6e63
Lazzrek <me.teichmann@gmail.com>
2019-02-06 19:26:37 +0100

    Fix handling of connection url

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,8 @@
 // Update with your config settings.
 
+
+
+
 module.exports = {
 
   development: {
@@ -28,9 +31,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
