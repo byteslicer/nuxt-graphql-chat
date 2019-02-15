8b7dd8336c96638cda1b36eaeab7957cde407a1c
Lazzrek <me.teichmann@gmail.com>
2019-02-15 11:18:24 +0100

    cleanup

--- migrations/20190203172528_init.js
+++ migrations/20190203172528_init.js
@@ -1,26 +1,22 @@
 
-exports.up = function(knex, Promise) {
-  return Promise.all([
-      knex.schema.createTable('users', table => {
-      table.uuid('id').primary().notNullable()
-      table.string('name').unique().notNullable()
-      table.string('password', 128).notNullable()
-      table.string('salt', 64).notNullable()
-      table.datetime('createdAt').defaultTo(knex.fn.now())
-    }),
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().notNullable();
+    table.string('name').unique().notNullable();
+    table.string('password', 128).notNullable();
+    table.string('salt', 64).notNullable();
+    table.datetime('createdAt').defaultTo(knex.fn.now());
+  }),
 
-    knex.schema.createTable('messages', table => {
-      table.uuid('id').primary().notNullable()
-      table.uuid('userId')
-      table.text('content')
-      table.datetime('createdAt').defaultTo(knex.fn.now())
-    })
-  ])
-};
+  knex.schema.createTable('messages', (table) => {
+    table.uuid('id').primary().notNullable();
+    table.uuid('userId');
+    table.text('content');
+    table.datetime('createdAt').defaultTo(knex.fn.now());
+  }),
+]);
 
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('users'),
-    knex.schema.dropTable('messages')
-  ])
-};
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.dropTable('users'),
+  knex.schema.dropTable('messages'),
+]);
