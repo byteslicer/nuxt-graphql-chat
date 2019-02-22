43aa2ef1d7f255ebe2677062424805f53f147dd1
Lazzrek <me.teichmann@gmail.com>
2019-02-22 11:44:59 +0100

    Added sentry

---
+++ plugins/sentry.client.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://a29c74260bf84715b57364644e0f8e8e@sentry.io/1400181',
+  integrations: [
+    new Sentry.Integrations.Vue({
+      Vue,
+      attachProps: true,
+    }),
+  ],
+});
