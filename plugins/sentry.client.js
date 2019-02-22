f32f7029424897074d3adea6a14c0a6eb7c9de5f
Lazzrek <me.teichmann@gmail.com>
2019-02-22 18:34:52 +0100

    Fixed sentry not sending release

--- plugins/sentry.client.js
+++ plugins/sentry.client.js
@@ -3,11 +3,11 @@ import * as Sentry from '@sentry/browser';
 
 Sentry.init({
   dsn: 'https://a29c74260bf84715b57364644e0f8e8e@sentry.io/1400181',
+  release: process.env.SOURCE_VERSION,
   integrations: [
     new Sentry.Integrations.Vue({
       Vue,
       attachProps: true,
-      release: process.env.SOURCE_VERSION,
     }),
   ],
 });
